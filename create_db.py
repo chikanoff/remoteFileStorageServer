0669c1f824705fe2eb2c8f46dbc7601faa3b5768
Pavel <nybkox@gmail.com>
2021-11-09 05:32:14 +0100

    fix roman's code

--- create_db.py
+++ create_db.py
@@ -1,17 +1,30 @@
+import json
 from conduit.app import create_app, db
-from fill_db import fill_db
+from conduit.models.user import User
+
+# from conduit.models.file import File
+
+
+def insert_table_from_json(path, model_cls):
+    app = create_app()
+    with app.app_context():
+        with open(path, "r", encoding="utf-8") as f:
+            rows = json.loads(f.read())
+            for row in rows:
+                db.session.add(model_cls(**row))
+            db.session.commit()
 
 
 def main():
     app = create_app()
 
     with app.app_context():
-        # pylint: disable=W0611
-
         db.drop_all()
         db.create_all()
 
-        fill_db()
+        # fill the table
+        insert_table_from_json("insert_data/users.json", User)
+        # insert_table_from_json("insert_data/files.json", File)
 
 
 if __name__ == "__main__":
